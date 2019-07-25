f88a0f63488cad08381afd136659b43a74441eb2
aviviaviavi <avinash.b392@gmail.com>
2019-07-25 23:34:04 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {Motion, spring} from 'react-motion';
+
 class App extends React.Component {
     constructor(props) {
       super(props);
 
       
-  this.state = {noofRow:0,cols:[],spaceRows:10,spaceBox:0};
-  this.updateRows = this.updateRows.bind(this);
-  this.updateCols = this.updateCols.bind(this);
+      this.state = {noofRow:0,cols:[],spaceRows:10,spaceBox:0};
+      this.updateRows = this.updateRows.bind(this);
+      this.updateCols = this.updateCols.bind(this);
 
-  this.updateRowspace = this.updateRowspace.bind(this);
+      this.updateRowspace = this.updateRowspace.bind(this);
 
-  this.updateColumnspace = this.updateColumnspace.bind(this);
+      this.updateColumnspace = this.updateColumnspace.bind(this);
 
 
 
@@ -59,7 +59,7 @@ class App extends React.Component {
     string+=`<div class="row" style=${this.rowstyle} >`;
     console.log(this.state.cols[i]);
       for(let j=0;j<this.state.cols[i]; j++){
-        string += `<div class="col" style=${this.columnstyle}></div>`;
+        string += `<div class="col" ></div>`;
       }
       string+=`</div>`;
   
@@ -73,8 +73,8 @@ class App extends React.Component {
     console.log(e.target.value);
 
     this.setState({spaceBox:e.target.value});
-    this.columnstyle = `margin:0px\t${e.target.value/2}px`;
-
+    // this.columnstyle = `margin:0px\t${e.target.value/2}px`;
+    e.target.style.setProperty('--x',e.target.value);
   }
 
     render() {
@@ -85,13 +85,10 @@ class App extends React.Component {
         <input className="inpt" onChange={this.updateCols} placeholder="enter no of columns ex:3,1,4"></input>
         <input className="inpt" onChange={this.updateRowspace} placeholder="enter space between rows ex:10"></input>
         <input className="inpt" onChange={this.updateColumnspace} placeholder="enter space between boxes ex:10"></input>
-          <Motion defaultStyle={{x:-200,opacity:0}} style={{x:spring(20),opacity:spring(1)}}>
-          {
-            (style) =>(<div id="parent" style={{transform:`translateX(${style.x}px)` ,opacity:style.opacity}}>
-            </div>)
-          }
-            
-          </Motion>
+
+          <div id="parent">
+          </div>
+    
         </div>
       );
     }
