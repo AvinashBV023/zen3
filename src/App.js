60968c9d85145ee472cbeae39273058c44b453a8
aviviaviavi <avinash.b392@gmail.com>
2019-07-25 19:44:50 +0530

    motion

--- src/App.js
+++ src/App.js
@@ -1,106 +1,100 @@
-import React, {useState} from 'react';
+import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-
+import {Motion, spring} from 'react-motion';
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+      super(props);
 
+      
+  this.state = {noofRow:0,cols:[],spaceRows:10,spaceBox:0};
+  this.updateRows = this.updateRows.bind(this);
+  this.updateCols = this.updateCols.bind(this);
+
+  this.updateRowspace = this.updateRowspace.bind(this);
+
+  this.updateColumnspace = this.updateColumnspace.bind(this);
+
+
+
+    }
+    markup;
+
+
+  updateRows(e){
     
-    this.state = {noofRow:0,cols:[],spaceRows:10,spaceBox:0};
-this.updateRows = this.updateRows.bind(this);
-this.updateCols = this.updateCols.bind(this);
+    this.setState({noofRow:e.target.value});
 
-this.updateRowspace = this.updateRowspace.bind(this);
+  
 
-this.updateColumnspace = this.updateColumnspace.bind(this);
+  }
+  updateCols(e){
+    let string='';
+
+
+  const colArray=e.target.value.split(',');
+  this.setState({cols:colArray});
 
 
 
   }
-  markup;
+  rowstyle;
+  columnstyle;
+  updateRowspace(e){
 
-
- updateRows(e){
-  
-  this.setState({noofRow:e.target.value});
-
- 
-
-}
-updateCols(e){
-  let string='';
-
-
- const colArray=e.target.value.split(',');
- this.setState({cols:colArray});
-
-
-
-}
-updateRowspace(e){
-  // let string='';
-
-console.log(e.target.value);
-  this.setState({spaceRows:e.target.value});
-
-
-}
-componentDidUpdate (){
-  let string='';
-
-
- 
-  for(let i=0;i<this.state.noofRow;i++){
-   string+=`<div class="row" style=${'margin:'+this.state.spaceRows+'px'} >`;
-   console.log(this.state.cols[i]);
-     for(let j=0;j<this.state.cols[i]; j++){
-       string += `<div class="col" style=${'margin:'+this.state.spaceBox+'px'}></div>`;
-     }
-     string+=`</div>`;
- 
- 
- }
- const node = document.querySelectorAll('#parent')[0];
- 
- node.innerHTML = string;
-}
-updateColumnspace(e){
-  // let string='';
   console.log(e.target.value);
+    this.setState({spaceRows:e.target.value});
+    this.rowstyle = `margin:${e.target.value}px\n0px`;
 
-  this.setState({spaceBox:e.target.value});
-  // for(let i=0;i<this.state.noofRow;i++){
-  //   string+=`<div class="row" style=${'margin:'+ this.state.spaceRows +'px 0px'} >`;
-  //     for(let j=0;j<this.state.cols[i]; j++){
-  //       string += `<div class="col" style=${'margin: 0px '+ e.target.value +'px'}></div>`;
-  //     }
-  //     string+=`</div>`;
-  
-  
-  // }
-  // const node = document.querySelectorAll('#parent')[0];
-  
-  // node.innerHTML = string;
+    console.log(this.rowstyle,'this.rowstyle');
 
-}
-
-  render() {
-    return (
-      <div className="container">
-        <h1 >Task</h1>
-       <input className="inpt" onChange={this.updateRows} placeholder="enter no of rows"></input>
-      <input className="inpt" onChange={this.updateCols} placeholder="enter no of columns"></input>
-      <input className="inpt" onChange={this.updateRowspace} placeholder="enter space between rows"></input>
-      <input className="inpt" onChange={this.updateColumnspace} placeholder="enter space between boxes"></input>
-
-        <div id="parent">
-       {/* {this.markup} */}
-        </div>
-   
-      </div>
-    );
   }
+ 
+  componentDidUpdate (){
+    let string='';
+
+
+  
+    for(let i=0;i<this.state.noofRow;i++){
+    string+=`<div class="row" style=${this.rowstyle} >`;
+    console.log(this.state.cols[i]);
+      for(let j=0;j<this.state.cols[i]; j++){
+        string += `<div class="col" style=${this.columnstyle}></div>`;
+      }
+      string+=`</div>`;
+  
+  
+  }
+  const node = document.querySelectorAll('#parent')[0];
+  
+  node.innerHTML = string;
+  }
+  updateColumnspace(e){
+    console.log(e.target.value);
+
+    this.setState({spaceBox:e.target.value});
+    this.columnstyle = `margin:0px\t${e.target.value/2}px`;
+
+  }
+
+    render() {
+      return (
+        <div className="container">
+          <h1 >Task</h1>
+        <input className="inpt" onChange={this.updateRows} placeholder="enter no of rows ex:3"></input>
+        <input className="inpt" onChange={this.updateCols} placeholder="enter no of columns ex:3,1,4"></input>
+        <input className="inpt" onChange={this.updateRowspace} placeholder="enter space between rows ex:10"></input>
+        <input className="inpt" onChange={this.updateColumnspace} placeholder="enter space between boxes ex:10"></input>
+          <Motion defaultStyle={{x:-200,opacity:0}} style={{x:spring(20),opacity:spring(1)}}>
+          {
+            (style) =>(<div id="parent" style={{transform:`translateX(${style.x}px)` ,opacity:style.opacity}}>
+            </div>)
+          }
+            
+          </Motion>
+        </div>
+      );
+    }
 }
 
 export default App;
